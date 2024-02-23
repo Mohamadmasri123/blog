fbeb2bc556a09f12f3f7cff983e21661d2a21012
Mohamad Masri <Mohamadmasri123>
2024-02-23 16:19:21 +0200

    edit  route

--- src/App.jsx
+++ src/App.jsx
@@ -19,6 +19,7 @@ const App = () => {
         <Routes>   
         
            
+        <Route path="/" element={<Home/>}/>
            <Route path="/home" element={<Home/>}/>
        
            <Route path="/Services" element={<Services/>}/>

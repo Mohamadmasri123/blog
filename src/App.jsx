4358b5c2bf275a0af744e7285e0a7835d352601f
Mohamad Masri <87850798+Mohamadmasri123@users.noreply.github.com>
2024-02-23 16:46:13 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -19,7 +19,7 @@ const App = () => {
         <Routes>   
         
            
-         <Route path="" element={<Home/>}/>
+         <Route path="/" element={<Home/>}/>
            <Route path="/home" element={<Home/>}/>
        
            <Route path="/Services" element={<Services/>}/>
